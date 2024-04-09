7b3f37e2a545189f45ac6b3337947fef5eea72ad
Benko3 <152974533+Benko3@users.noreply.github.com>
2024-04-09 12:17:00 +0200

    Add files via upload

---
+++ klasadirectory.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasa_directory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // unos putanje mape
+            Console.WriteLine("Unesi putanju mape: ");
+            string putanja = Console.ReadLine();
+            // provjera da li mapa postoji
+            if (Directory.Exists(putanja))
+            {
+                Console.WriteLine("Mapa postoji! ");
+                // ako postoji ispisuje sve datoteke u mapi
+                Console.WriteLine("Ispis podataka:");
+                foreach (string datoteka in Directory.GetFiles(putanja))
+                {
+                    Console.WriteLine(datoteka);
+                }
+                string putanjaPodmape = putanja + "\\bla";
+
+                // provjera da li podmapa postoji, ako ne postoji, kreiramo je
+                if (!Directory.Exists(putanjaPodmape))
+                {
+                    Directory.CreateDirectory(putanjaPodmape);
+                    Console.WriteLine("Mapa " + putanjaPodmape + " je napravljena.");
+                }
+                // ispis podmapa
+                Console.WriteLine("Ispis podmapa:");
+                foreach(string podmapa in Directory.GetDirectories(putanja))
+                {
+                    Console.WriteLine(podmapa);
+                }
+                // brisanje podmape
+                Directory.Delete(putanjaPodmape);
+                Console.WriteLine("Ispis podmapa nakon brisanja:");
+                foreach (string podmapa in Directory.GetDirectories(putanja))
+                {
+                    Console.WriteLine(podmapa);
+                }
+            }
+            // ako ne postoji, kreiramo mapu
+            else
+            {
+                Directory.CreateDirectory(putanja);
+                Console.WriteLine("Mapa " + putanja + " je napravljena!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
